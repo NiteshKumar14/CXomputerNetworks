9ff5272f3e1b52c24101fad73158854c5435dc6d
Nitesh Kumar <erniteshch@gmail.com>
2018-09-20 10:42:05 +0530

    Add files via upload

--- Crc.cpp
+++ Crc.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void crc(int *m,int *g,int msize,int gsize)
+string crc(int *m,int *g,int msize,int gsize)
 {   
     int count=0;
     int flag=0,k=0,i=count;
     bool choice=false;
     int gs=gsize;
     string result="";
+   
     while(gs!=msize+gsize)
     {   
         
-         cout<<"count ="<<count<<endl;
-        cout<<"gs "<<gs<<endl;
+         //cout<<"count ="<<count<<endl;
+        //cout<<"gs "<<gs<<endl;
             if(m[i]==0)
             {
                 for(int j=count;j<gs;j++)
                 {
                     m[j]=m[j]^0;
-                    cout<<" zore xor "<<m[j]<<endl;
+                    //cout<<" zore xor "<<m[j]<<endl;
                     
                 }
                result+="0";
@@ -27,7 +28,7 @@ void crc(int *m,int *g,int msize,int gsize)
                     count++;
                     choice =true;
                 }
-               */ cout<<"flag"<<flag<<endl;
+               */ //cout<<"flag"<<flag<<endl;
                
             }
              if(m[i]==1)
@@ -35,7 +36,7 @@ void crc(int *m,int *g,int msize,int gsize)
                 for(int j=count;j<gs;j++)
                 {
                     m[j]=m[j]^g[k++];
-                    cout<<"1 xor "<<m[j]<<endl;
+                    //cout<<"1 xor "<<m[j]<<endl;
                    
                 }
                 result+="1";
@@ -54,9 +55,28 @@ void crc(int *m,int *g,int msize,int gsize)
         
         
     }
-    cout<<"result "<<result<<endl;
+    return result;
 
 }
+void display_rem(int *c,int size)
+{ int flag=0,i=0;
+    while(c[i]!=1&&i<size)
+    {i++;}
+    for(;i<size;i++)
+    cout<<c[i];
+    cout<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
 void display(int *c,int size)
 {
     for(int i=0;i<size;i++)
@@ -65,19 +85,27 @@ void display(int *c,int size)
     }
     cout<<endl;
 }
+void arr_Copy(int *source,int *destination,int size )
+{
+    for(int i=0;i<size;i++)
+    {
+        destination[i]=source[i];
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     string msg="",gen="";
-    int *mrr,*grr;
+    int *mrr,*grr,*arr;
     cout<<"enter msg"<<endl;
     cin>>msg;
     cout<<"enter generator"<<endl;
     cin>>gen;
      int size=msg.size()+(gen.size())-1;
-     cout<<"size"<<size<<endl;
+     //cout<<"size"<<size<<endl;
     mrr=new int[size];
     grr=new int[gen.size()];
+    arr=new int[size];
    
     for(int i=0;i<msg.size();i++)
     {
@@ -89,21 +117,29 @@ int main(int argc, char const *argv[])
     {
         mrr[i]=0;
     }
-
+ arr_Copy(mrr,arr,size);
     
-    cout<<"DATA = ";
-    display(mrr,size);
+    
     
 
     for(int i=0;i<gen.size();i++)
     {
      grr[i]=gen[i]-'0';
     }
-    crc(mrr,grr,msg.size(),gen.size());
-    cout<<"remainder";
-    display(mrr,msg.size()+(gen.size())-1);
-    
-    
+    string result=crc(mrr,grr,msg.size(),gen.size());
+    cout<<"remainder :";
+    display_rem(mrr,msg.size()+(gen.size())-1);
+    for(int i=0;i<size;i++)
+    {
+        arr[i]+=mrr[i];
+        
+    }
+    cout<<"result is :"<<result<<"\n data transmitted is : ";
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i];
+    }
+    cout<<endl;
     
     return 0;
 }
